4e152773df26ab1adba3d671151fda08c482dfb3
Levko Kravets <levko.ne@gmail.com>
2016-11-20 23:31:49 +0200

    Export tartan library as an Angular service

--- src/module.js
+++ src/module.js
@@ -1,7 +1,10 @@
 'use strict';
 
+var tartan = require('tartan');
 var angular = require('angular');
 
 var ngTartan = angular.module('angular-tartan', []);
+
+ngTartan.constant('tartan', tartan);
 
 module.exports = ngTartan;
